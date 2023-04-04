981dc13cce44e141174b5d7190609df5d1000192
tonydube <126003571+tonydube@users.noreply.github.com>
2023-04-04 12:54:56 -0400

    [vvt] add refresh token functionality to express server

--- server/server.js
+++ server/server.js
@@ -10,7 +10,23 @@ app.use(express.json())
 
 const users = []
 
-app.get('/users', (req, res) => {
+let refreshTokens = []
+
+app.post('/token', (req, res) => {
+  console.log(refreshTokens)
+  const refreshToken = req.body.token
+  console.log('token', refreshToken)
+  if (refreshToken == null) return res.sendStatus(401)
+  if (refreshTokens.includes(refreshToken)) return res.sendStatus(403)
+  console.log('here')
+  jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403)
+    const accessToken = generateAccessToken({ username: user.username })
+    res.json({ accessToken: accessToken })
+  })
+})
+
+app.get('/users', authenticateToken, (req, res) => {
   res.json(users)
 })
 
@@ -26,32 +42,37 @@ app.post('/users', async (req, res) => {
   }
 })
 
-app.post('/users/login', async (req, res) => {
-  try {
-    const user = users.find(user => user.name = req.body.name)
-    if(user == null) {
-      return res.status(400).send('Cannot find user')
-    }
-    if(await bcrypt.compare(req.body.password, user.password)) {
-      res.send('Success')
-    } else {
-      res.send('Not allowed')
-    }
-  } catch {
-    res.status(500).send()
-  }
-});
+// app.post('/users/login', async (req, res) => {
+//   try {
+//     const user = users.find(user => user.name = req.body.name)
+//     if(user == null) {
+//       return res.status(400).send('Cannot find user')
+//     }
+//     if(await bcrypt.compare(req.body.password, user.password)) {
+//       res.send('Success')
+//     } else {
+//       res.send('Not allowed')
+//     }
+//   } catch {
+//     res.status(500).send()
+//   }
+// });
 
-app.post('/login', authenticateToken, (req, res) => {
+app.delete('/logout', (req, res) => {
+  refreshTokens = refreshTokens.filter(token => token !== req.body.token)
+  res.sendStatus(204)
+})
+
+app.post('/login', (req, res) => {
   // authenticate user
 
-  const username = req.body.username;
-  console.log(username)
+  const username = req.body.username
   const user = { name: username }
-  console.log(user)
 
-  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
-  res.json({ accessToken: accessToken })
+  const accessToken = generateAccessToken(user)
+  const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET)
+  refreshTokens.push(refreshToken)
+  res.json({ accessToken: accessToken, refreshToken: refreshToken })
 })
 
 function authenticateToken(req, res, next) {
@@ -64,6 +85,10 @@ function authenticateToken(req, res, next) {
     req.user = user
     next()
   })
+}
+
+function generateAccessToken(user) {
+  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '15s'})
 }
 
 app.listen(3000)
